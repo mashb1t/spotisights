adc55404a4eb37be732b0cd4d1183a7941e9a368
Manuel Schmid <manuel.schmid@odt.net>
2022-01-30 03:32:46 +0100

    feat: add exception handling to a part of callback.php

--- php/src/public/callback.php
+++ php/src/public/callback.php
@@ -1,16 +1,21 @@
 <?php
 
 use App\Factory;
+use SpotifyWebAPI\SpotifyWebAPIException;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 session_start();
 
-$state = $_GET['state'];
+$state = $_GET['state'] ?? null;
+$sessionState = $_SESSION['state'] ?? null;
+
+$code = $_GET['code'] ?? null;
 
 // check state integrity
-if ($state !== $_SESSION['state']) {
-    die('State mismatch');
+if (!$state || $state !== $sessionState || !$code) {
+    header('refresh:5;url=index.php');
+    die('State mismatch or not authenticated, redirecting to login...');
 }
 
 $factory = new Factory();
@@ -19,7 +24,16 @@ $session = $factory->getSession();
 $spotifyWebAPI = $factory->getSpotifyWebAPI($session);
 $sessionHandler = $factory->getSessionHandler();
 
-$session->requestAccessToken($_GET['code']);
+$accessTokenCreated = false;
+try {
+    $accessTokenCreated = $session->requestAccessToken($code);
+} catch (SpotifyWebAPIException $exception) {
+} finally {
+    if (!$accessTokenCreated) {
+        header('refresh:5;url=index.php');
+        die('Access token could not be created, redirecting to login...');
+    }
+}
 
 // set refreshToken to redirect directly from index to app without redirect to spotify
 $_SESSION['refreshToken'] = $session->getRefreshToken();
@@ -27,22 +41,23 @@ $_SESSION['refreshToken'] = $session->getRefreshToken();
 // todo add exception handling
 $_SESSION['username'] = $spotifyWebAPI->me()->id;
 
-$sessionHandler->saveSession($session, $_SESSION['username']);
+$dashboardUrl = getenv('GRAFANA_DASHBOARD_URL');
 
-$dashboardUrl =getenv('GRAFANA_DASHBOARD_URL');
+if (!$sessionHandler->sessionExists($_SESSION['username'])) {
+    $sessionHandler->saveSession($session, $_SESSION['username']);
 
-if (!$dashboardUrl) {
-    echo 'All set up for user ' . $_SESSION['username'] . ', let the cronjob do the rest!';
-    die();
+    $trackHistoryCrawler = $factory->getTrackHistoryCrawler($session);
+    $trackHistoryCrawler->crawl($_SESSION['username']);
+
+    // todo idea: create new grafana user via API https://grafana.com/docs/grafana/latest/http_api/admin/#global-users and display password once?
+
+    header("refresh:5;url=$dashboardUrl");
+    die('All set up for user ' . $_SESSION['username'] . ', redirecting to dashboard...');
 }
 
-?>
-<html lang="en-us">
-<head>
-    <meta http-equiv="refresh" content="3;url=<?= $dashboardUrl ?>">
-</head>
-<body>
-    <p>All set up for user <?= $_SESSION['username'] ?>, let the cronjob do the rest!</p>
-    <p>Redirecting to dashboard...</p>
-</body>
-</html>
+if (!$dashboardUrl) {
+    die('All set up for user ' . $_SESSION['username'] . ', let the cronjob do the rest!');
+}
+
+header("refresh:5;url=$dashboardUrl");
+die('All set up for user ' . $_SESSION['username'] . ', let the cronjob do the rest! Redirecting to dashboard...');
