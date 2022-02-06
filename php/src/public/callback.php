c0e3248a3e836b231822c9327cf238ab50c1590a
Manuel Schmid <manuel.schmid@odt.net>
2022-02-06 20:40:52 +0100

    fix: use strict types when checking SESSION_SETUP_SUCCESS
    $crawlerInitialSetup == CrawlerResultEnum::SESSION_SETUP_SUCCESS) could never be true due to array vs string comparison

--- php/src/public/callback.php
+++ php/src/public/callback.php
@@ -37,17 +37,15 @@ $username = $_SESSION[$crawler->getType() . '_username'] ?? null;
 // do initial crawl
 // TODO extract to separate method/class
 $initialSetupResult = $crawler->initialSetup($username, ['code' => $code]);
-$crawlerInitialSetup[$crawler->getType()] = $initialSetupResult;
 
-if ($initialSetupResult == CrawlerResultEnum::SESSION_ACCESS_TOKEN_ERROR) {
+if ($initialSetupResult === CrawlerResultEnum::SESSION_ACCESS_TOKEN_ERROR) {
     $_SESSION['logged_in'][$crawler->getType()] = false;
     header('refresh:5;url=index.php');
     die(CrawlerResultEnum::SESSION_ACCESS_TOKEN_ERROR->value);
-} else if ($crawlerInitialSetup == CrawlerResultEnum::SESSION_SETUP_SUCCESS) {
+} else if ($initialSetupResult === CrawlerResultEnum::SESSION_SETUP_SUCCESS) {
     try {
         // read new username from session if now set by initial setup
         $crawler->crawlAll($_SESSION[$crawler->getType() . '_username']);
-        $crawlerResult[$crawler->getType()] = true;
     } catch (Exception $exception) {
         $crawlerResult[$crawler->getType()] = CrawlerResultEnum::CRAWL_FAILED;
         die($exception->getMessage());
