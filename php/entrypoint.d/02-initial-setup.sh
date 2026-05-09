7c23bd0190e4ee6c974ed8ec2e35b83a7c998973
Manuel Schmid <dev@mashb1t.de>
2026-05-09 22:56:46 +0200

    fix: move artisan build-time calls to entrypoint (amd64 php 8.1 segfault)

--- php/entrypoint.d/02-initial-setup.sh
+++ php/entrypoint.d/02-initial-setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
+/usr/local/bin/php /app/artisan package:discover --ansi
+/usr/local/bin/php /app/artisan storage:link
 /usr/local/bin/php /app/artisan migrate --force
 /usr/local/bin/php /app/artisan crawl:active
